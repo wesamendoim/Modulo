03b658ca9a3d4457c9044bb9e8ec8f6e59da3909
wesamendoim <30731163+wesamendoim@users.noreply.github.com>
2025-03-26 23:23:28 +0000

    MODULOS

---
+++ items/cadastro.py
@@ -0,0 +1,5 @@
+def cliente():
+  print('Cadastro de Cliente')
+
+
+  
---
+++ main.py
@@ -0,0 +1,9 @@
+# import funcao
+
+# funcao.somar()
+# funcao.multi()
+
+from funcao import somar, multi
+from items.cadastro import cliente
+
+cliente()
